9872a6b2057893deac75b86b997c408f2db7c6b9
TheB1t0s <coimbrawork25@gmail.com>
2023-03-07 17:14:44 -0300

    Projeto Hex

---
+++ WebMySQL/Models/Usuario.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMySQL.Models
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Display(Name = "Id")]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Column("Nome")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Idade")]
+        [Column("Idade")]
+        public string Idade { get; set; }
+
+        [Display(Name = "Civil")]
+        [Column("Civil")]
+        public string Civil { get; set; }
+
+        [Display(Name = "CPF")]
+        [Column("CPF")]
+        public string CPF { get; set; }
+
+        [Display(Name = "Estado")]
+        [Column("Estado")]
+        public string Estado { get; set; }
+
+        [Display(Name = "Cidade")]
+        [Column("Cidade")]
+        public string Cidade { get; set; }
+    }
+}
